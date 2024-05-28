eac6720d23d073bb960fae93af9c76cafb1f4e35
bin888 <zenith@elascription.net>
2024-05-28 19:18:30 +0800

    modify registerArbitrator.ts

--- scripts/registerArbitrator.ts
+++ scripts/registerArbitrator.ts
@@ -2,6 +2,7 @@
 import { network, ethers, upgrades, getChainId } from 'hardhat'
 import {readConfig} from "./helper";
 import {BigNumber} from "ethers";
+import {publicKeyCreate} from 'secp256k1';
 
 async function main() {
     let chainID = await getChainId();
@@ -15,18 +16,28 @@ async function main() {
     let contract  = await contractFactory.connect(account).attach(contractAddress);
 //uint256 _commitPeriod, bytes calldata _btcPublicKey, address _token, uint256 _amount
     let _commitPeriod = 30 * 3600 * 24;
-    let btcPublicKey = "0x0200493eb975eedf5d5d2f7f5458e790e8264576ec137df06c8f3f90c91b0a6f78";
+
+    const privateKeys = network.config.accounts;
+    const signerPrivate = privateKeys[0];
+    let privateKeyHex = signerPrivate.substr(2);
+    console.log("private key ", privateKeyHex);
+    const pubKey = publicKeyCreate(Buffer.from(privateKeyHex, "hex"), true);
+    const publicKeyBytes = Buffer.from(pubKey, 'hex');
+    console.log("public key ", publicKeyBytes.toString("hex"))
+
     let usdt = "0x0daddd286487f3a03Ea9A1b693585fD46cdCcF9F";
     if(chainID == 21) {
         usdt = "0x892A0c0951091A8a072A4b652926D4A8875F9bcB";
     }
-
-    let amount = BigNumber.from("1000000000000000000");//10 * 1e18;
-    amount = amount.mul(10);
-    let tx = await contract.registerArbitrator(_commitPeriod, btcPublicKey, usdt, amount);
+    let amount = BigNumber.from("1000000000000000000");
+    amount = amount.mul(10);//10 * 1e18;
+    let tx = await contract.registerArbitrator(_commitPeriod, publicKeyBytes, usdt, amount);
     console.log("register arbitrator =", tx.hash);
     let receipt = await  tx.wait();
     console.log("receipt=", receipt);
+
+    let arbitratorPbk = await contract.getArbitratorPublicKey();
+    console.log("arbitratorPbk ", arbitratorPbk);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
